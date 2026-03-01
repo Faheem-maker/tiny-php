3eaac77262c0de115b6177912b5ea815bf773b25
Faheem Anis <faheem@cartzlink.com>
2026-03-01 14:27:07 +0500

    Title Card Widget
    A simple, KPI-like card with a title and optional value

--- src/app/assets/widgets/ui/titleCard/TitleCard.php
+++ src/app/assets/widgets/ui/titleCard/TitleCard.php
@@ -8,12 +8,14 @@ class TitleCard extends Widget{
     public $color = 'primary';
     public $title = '';
     public $data_text = '';
+    public $text = '';
 
     public function run() {
         return $this->renderPartial('title_card', [
             'color' => $this->color,
             'title' => $this->title,
             'data_text' => $this->data_text,
+            'text' => $this->text,
         ]);
     }
 }
