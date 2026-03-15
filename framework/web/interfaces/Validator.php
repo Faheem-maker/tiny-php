550a81e12f407aaf60330cd5ccb7453a6b32361f
Faheem Anis <faheem@cartzlink.com>
2026-03-15 15:42:12 +0500

    Add var interpolation for validation attributes

--- framework/web/interfaces/Validator.php
+++ framework/web/interfaces/Validator.php
@@ -13,7 +13,13 @@ abstract class Validator {
     }
 
     public function message() {
-        return $this->message;
+        $msg = $this->message;
+        foreach (get_object_vars($this) as $key => $value) {
+            if (is_scalar($value)) {
+                $msg = str_replace("{" . $key . "}", $value, $msg);
+            }
+        }
+        return $msg;
     }
 
     public abstract function validate($value, $doc = null): bool;
