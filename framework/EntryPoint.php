2bbd5f23cafda5e8910d912385af7f44b31dec73
Faheem Anis <faheem@cartzlink.com>
2026-03-22 15:04:45 +0500

    Add not found exception screen

--- framework/EntryPoint.php
+++ framework/EntryPoint.php
@@ -2,12 +2,17 @@
 
 namespace framework;
 
+use framework\web\exceptions\NotFoundException;
 use framework\web\Routes;
 
 class EntryPoint {
     public function run($method) {
         $app = Application::get();
         $route = Routes::resolve($app->url->path(), $method);
+
+        if (empty($route)) {
+            throw new NotFoundException();
+        }
 
         $result = $route['route']->execute($route['params']);
 
