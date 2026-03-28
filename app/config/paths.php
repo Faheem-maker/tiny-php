420aed3dbba379eb235e726736f90a7a9467115c
Faheem Anis <faheem@cartzlink.com>
2026-03-28 20:17:57 +0500

    Added "app" within paths

--- app/config/paths.php
+++ app/config/paths.php
@@ -5,6 +5,7 @@ $base_dir = realpath(dirname(__DIR__, 2));
 
 return [
     'base_dir' => $base_dir,
+    'app' => $base_dir . DS . 'app',
     'assets' => $base_dir . DS . 'app' . DS . 'resources',
     'views' => $base_dir . DS . 'app' . DS . 'resources' . DS . 'views',
     'widgets' => $base_dir . DS . 'app' . DS . 'resources' . DS . 'widgets',
