922e87d809e22975fd719e4e805911274d8d7dd3
Faheem Anis <faheem@cartzlink.com>
2026-03-28 20:24:05 +0500

    Added CLI commands for code generation (#14)
    * Added CLI integration for controller/model creation
    * Added "app" within paths

---
+++ cli/config.php
@@ -0,0 +1,25 @@
+<?php
+
+use framework\Application;
+
+/**
+ * Thie file bootstraps the config container and load all
+ * configuration files automatically
+ * 
+ * @var Application $app
+ */
+use framework\components\Config;
+use framework\utils\helpers\DirectoryHelper;
+use framework\utils\helpers\DotenvHelper;
+
+// Load dotenv
+DotenvHelper::load(__DIR__ . '/../.env');
+
+$app->registerComponent('config', new Config());
+
+foreach (DirectoryHelper::listFiles(__DIR__ . '/../app/config', 'web.php') as $file) {
+    $res = require_once __DIR__ . '/../app/config/' . $file;
+    if (!empty($res)) {
+        $app->config->set(pathinfo($file, PATHINFO_FILENAME), $res);
+    }
+}
