61214eca5ec3ee35917d0c8535fc1994028213d0
Faheem Anis <faheem@cartzlink.com>
2026-04-03 19:52:21 +0500

    Corrected namespace for Csrf

--- app/http/middleware/Csrf.php
+++ app/http/middleware/Csrf.php
@@ -5,10 +5,12 @@ namespace app\http\middleware;
 use Exception;
 use framework\web\request\Request;
 
-class Csrf {
-    public function handle($next, Request $request) {
+class Csrf
+{
+    public function handle($next, Request $request)
+    {
         if ($request->method() != 'GET') {
-            if (!\framework\utils\security\Csrf::validate($request->post('_csrf'))) {
+            if (!\framework\web\utils\security\Csrf::validate($request->post('_csrf'))) {
                 throw new Exception("Unable to verify CSRF token");
             }
         }
