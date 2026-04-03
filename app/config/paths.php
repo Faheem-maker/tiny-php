a8fa85b4e5f43c0275ea25acf0c416e5c3e94cae
Faheem Anis <faheem@cartzlink.com>
2026-04-03 19:59:34 +0500

    Additional paths for storage/resources

--- app/config/paths.php
+++ app/config/paths.php
@@ -6,6 +6,8 @@ $base_dir = realpath(dirname(__DIR__, 2));
 return [
     'base_dir' => $base_dir,
     'assets' => $base_dir . DS . 'app' . DS . 'resources',
+    'resources' => $base_dir . DS . 'app' . DS . 'resources',
+    'storage' => $base_dir . DS . 'app' . DS . 'storage',
     'views' => $base_dir . DS . 'app' . DS . 'resources' . DS . 'views',
     'widgets' => $base_dir . DS . 'app' . DS . 'resources' . DS . 'widgets',
     'runtime' => $base_dir . DS . 'app' . DS . 'runtime',
